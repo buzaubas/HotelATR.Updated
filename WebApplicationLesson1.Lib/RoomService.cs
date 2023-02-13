15e8d895dd8ae6a9be4335cd8361978149f168b1
Dimash Bexultanov <94544861+buzaubas@users.noreply.github.com>
2023-02-13 21:52:12 +0600

    Add files via upload

---
+++ WebApplicationLesson1.Lib/RoomService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebApplicationLesson1.Lib
+{
+    public class RoomService
+    {
+        ModelEntity db = new ModelEntity();
+        public bool AddRoomProperties(RoomProperty roomProperty) 
+        {
+            try
+            {
+                db.RoomProperty.Add(roomProperty);
+                db.SaveChanges();
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+}
